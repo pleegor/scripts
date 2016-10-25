109dcdf257848551747089ee694bc5f05391e123
Igor Platitsa <pl33g0r@gmail.com>
2016-10-24 18:03:40 -0700

    added parsing of CLI arguments

--- uberbackup.py
+++ uberbackup.py
@@ -2,12 +2,23 @@ import boto3
 import datetime
 import shutil
 import os
+import argparse
 
-# Creates archive of jenkins home into .tar, pushes archive into s3,
-# and deletes all archived files locally besides the latest one. You
-# need to have awscli and boto3 installed and pre-configured
+# Creates archive of jenkins home and pushed archive into s3
 
 today = datetime.date.today()
+parser = argparse.ArgumentParser(description='This script will create '
+                                             'an archive and will push '
+                                             'it into S3 bucket')
+parser.add_argument('-destination', type=str,
+                    help='enter path where new archive will be stored')
+parser.add_argument('-target', type=str,
+                    help='enter location of the directory '
+                         'that will be archived')
+# parser.add_argument('-bucket-name', type=str,
+#                     help='enter S3 bucket name where archive will '
+#                          'be pushed to')
+args = parser.parse_args()
 
 
 
@@ -16,15 +27,15 @@ def build_archive(name):
     """
      This function will create an archive of Jenkins's home directory
     :param name:  name of the archive
+    :param directory: location of Jenkins home directory
     :return: archived
     '''
     """
 
     archive_name = name + str(today)
-    archived_location = <Path>
-    #archived_location is path where archive will be stored
-    archiving_dir = <path>
-    #archived_dir is path where archiving file is currently located
+    archived_location = args.destination
+    #print(archive_name)
+    archiving_dir = args.target
     print('Starting archiving Jenkins home located at:'  + " " +
        archiving_dir)
     archived = shutil.make_archive(archived_location + archive_name,
@@ -37,11 +48,11 @@ def push_to_s3(archive, bucket_name):
 
     """
     :param bucket_name: Name of the bucket where archive is uploaded
-    :return: uploaded
+    :return:
     """
     print('Starting upload to S3')
     s3 = boto3.resource('s3')
-    file_name = <name> + str(today) + '.tar'
+    file_name = 'uberjenkins' + str(today) + '.tar'
     uploaded = s3.Bucket(bucket_name).upload_file(
         archive,
         file_name
@@ -54,10 +65,9 @@ def remove_old_archive(file_name):
     '''
     This function enforces only one archive to be stored in /tmp
     :param file_name:
-    :return: file
+    :return:
     '''
-    #provide path where archives are locally stored
-    path = <path>
+    path = args.destination
     item_list = os.listdir(path)
     for file in item_list:
         if file != file_name + str(today) + '.tar':
@@ -69,6 +79,6 @@ def remove_old_archive(file_name):
     return file
 
 
-archive = build_archive(<name>)
-upload = push_to_s3(archive, <bucket name>)
-delete = remove_old_archive(<name>)
+archive = build_archive('uberjenkins')
+upload = push_to_s3(archive, 'cbit-logging')
+delete = remove_old_archive('uberjenkins')
